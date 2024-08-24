0db939a4b4479e908506a5c1b72d83442cc4d92a
Fn-front <reservoir-station@outlook.jp>
2024-08-24 17:11:28 +0900

    transition state処理

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 export default function Home() {
 
   const [animation, setAnimation] = useState<boolean>(false)
+  const [transition, setTransition] = useState<boolean>(false)
 
   return (
     <>
@@ -15,9 +16,9 @@ export default function Home() {
       </div>
 
       <h2 className="c_h2">Transition @starting-style 設定あり</h2>
-      <button className="c_button">onClick!</button>
+      <button className="c_button" onClick={() => setTransition((prev) => !prev)}>onClick!</button>
       <div className="c_starting_style">
-        <div className="c_starting_style_item"></div>
+        <div className={`c_starting_style_item ${transition ? 'c_starting_style_item_ver_transition' : ''}`}></div>
       </div>
 
       <h2 className="c_h2">Transition @starting-style 設定なし</h2>
