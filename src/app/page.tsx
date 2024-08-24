3f08e389b40f8919908145b475cd0d1c348f60c1
Fn-front <reservoir-station@outlook.jp>
2024-08-24 15:48:33 +0900

    不要dom削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <>
-      <h2 className="c_h2">テンプレ</h2>
       <div className="c_starting_style">
         <div className="c_starting_style_item"></div>
       </div>
