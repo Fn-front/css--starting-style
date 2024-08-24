d8bb47144b449d2400c308677268f921d5b1d12e
Fn-front <reservoir-station@outlook.jp>
2024-08-24 15:48:18 +0900

    dom追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <>
       <h2 className="c_h2">テンプレ</h2>
+      <div className="c_starting_style">
+        <div className="c_starting_style_item"></div>
+      </div>
     </>
   );
 }
