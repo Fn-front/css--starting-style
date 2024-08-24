dd9971d00c9eb6b43bad657836d48ef0daafe65e
Fn-front <reservoir-station@outlook.jp>
2024-08-24 16:16:30 +0900

    animationとtransitionに分割

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 export default function Home() {
   return (
     <>
+      <h2 className="c_h2">animationは、display: noneでも動く</h2>
+      <div className="c_starting_style">
+        <div className="c_starting_style_item c_starting_style_item_ver_animation"></div>
+      </div>
+
+      <h2 className="c_h2">Transition @starting-style 設定あり</h2>
+      <div className="c_starting_style">
+        <div className="c_starting_style_item"></div>
+      </div>
+
+      <h2 className="c_h2">Transition @starting-style 設定なし</h2>
       <div className="c_starting_style">
         <div className="c_starting_style_item"></div>
       </div>
